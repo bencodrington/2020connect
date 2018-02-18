14ea759c4ba6df802d96ae45d44c292beb126925
Ben <benvenda@gmail.com>
2018-02-18 15:41:55 -0500

    Added basic unity files, updated .gitignore to match folder structure, added TileMap and TileType classes.

---
+++ 2020 Connect/Assets/Scripts/TileMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+	TileType[] tileTypes;
+
+	int[,] tiles;
+
+	int mapSizeX = 10;
+	int mapSizeY = 10;
+
+	void Start() {
+		// Allocate space for tiles
+		tiles = new int[mapSizeX, mapSizeY];
+
+		// Instantiate each tile to type 0 by default
+		for (int x = 0; x < mapSizeX; x++) {
+			for (int y = 0; y < mapSizeY; y++) {
+				tiles[x, y] = 0;
+			}
+		}
+	}
+
+}
